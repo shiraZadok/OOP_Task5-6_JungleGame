fe3ffc6f2d80fb5227cbaccf5502406e02c3e939
shirazadok <shira19941@gmail.com>
2020-01-19 20:47:28 +0200

    finish JUNIT

--- out/test/Graphs/README.md
+++ out/test/Graphs/README.md
@@ -1,12 +1,15 @@
-![openGame](https://user-images.githubusercontent.com/58064644/72602430-efe8c400-391f-11ea-886c-68d487501bac.png)
+![Jungle-Game](https://user-images.githubusercontent.com/58064644/72680447-a2f12300-3ac2-11ea-86b5-a3ebffd9234b.png)
+
 ##### Submit: Yirat Peleg & Shira Zadok
 
 # The Jungle Game
 In this project we build a Jungle Game.
 The game based on Grahs with nodes and edges taht represent the route. The player in the game represented by moneky. There are fruits scattered on the graph randomly and the goal of the game is to eat as much fruit as possible and earn as many points as possible.
+
 In the game there is two option - Automatical game or Manual game. The player is asked before the play what he prefers. If he chose in Manual game - he needs to have a good strategy for navigating the graph best so he can eat as much fruit as possible.
 If he chose in automatical game - we have programmed an algorithm that is based on the shortest path that exists between a certain monkey in graph and fruit.
 There is few level in the game at a rising difficulty level.
+
 Ia addition, there is an option at the end of the game to see it on GoogleEarth.
 
 The game draw by STDraw class.
@@ -20,7 +23,6 @@ The game draw by STDraw class.
 ![modeGame](https://user-images.githubusercontent.com/58064644/72608540-9090b100-392b-11ea-8999-4e676ce7c836.png)
 
 ### Visual game 
-![visualGame](https://user-images.githubusercontent.com/58064644/72608538-9090b100-392b-11ea-8080-0435cc2073e8.png)
 
 
 ### Diagram structure 
--- src/Tests/RobotsTest.java
+++ src/Tests/RobotsTest.java
@@ -10,6 +10,7 @@ public class RobotsTest {
 
     static Robots[] checkforBuildRobot = new Robots[8];
     static Point3D[] checkPoint3D = new Point3D[8];
+    static String[] checkJsonString = new String[8];
 
     @Before
     public void BeforeEach() {
@@ -31,10 +32,31 @@ public class RobotsTest {
         checkforBuildRobot [5] = new Robots(6,6,6,checkPoint3D[5],6,6,"monkey.png");
         checkforBuildRobot [6] = new Robots(7,7,7,checkPoint3D[6],7,7,"monkey.png");
         checkforBuildRobot [7] = new Robots(8,8,8,checkPoint3D[7],8,8,"monkey.png");
+
+        checkJsonString[0] = new String("{\"Robot\":{\"src\":1,\"pos\":\"1.0,2.0,0.0\",\"id\":1,\"dest\":1,\"value\":1.0,\"speed\":1}}");
+        checkJsonString[1] = new String("{\"Robot\":{\"src\":2,\"pos\":\"3.0,4.0,0.0\",\"id\":2,\"dest\":2,\"value\":2.0,\"speed\":2}}");
+        checkJsonString[2] = new String("{\"Robot\":{\"src\":3,\"pos\":\"5.0,6.0,0.0\",\"id\":3,\"dest\":3,\"value\":3.0,\"speed\":3}}");
+        checkJsonString[3] = new String("{\"Robot\":{\"src\":4,\"pos\":\"7.0,8.0,0.0\",\"id\":4,\"dest\":4,\"value\":4.0,\"speed\":4}}");
+        checkJsonString[4] = new String("{\"Robot\":{\"src\":5,\"pos\":\"9.0,10.0,0.0\",\"id\":5,\"dest\":5,\"value\":5.0,\"speed\":5}}");
+        checkJsonString[5] = new String("{\"Robot\":{\"src\":6,\"pos\":\"11.0,12.0,0.0\",\"id\":6,\"dest\":6,\"value\":6.0,\"speed\":6}}");
+        checkJsonString[6] = new String("{\"Robot\":{\"src\":7,\"pos\":\"13.0,14.0,0.0\",\"id\":7,\"dest\":7,\"value\":7.0,\"speed\":7}}");
+        checkJsonString[7] = new String("{\"Robot\":{\"src\":8,\"pos\":\"15.0,16.0,0.0\",\"id\":8,\"dest\":8,\"value\":8.0,\"speed\":8}}");
+
     }
 
     @Test
     public void init() {
+        Robots r = new Robots();
+        for (int i = 0; i<checkJsonString.length; i++){
+            r = (Robots)r.init(checkJsonString[i]);
+            assertEquals(i+1, r.getSrc());
+            assertEquals(i+1, r.getDest());
+            assertEquals(i+1, r.getValue(), 0.0001);
+            assertEquals(i+1, r.getId());
+            assertEquals((i*2)+1, r.getLocation().x(), 0.0001);
+            assertEquals((i*2)+2, r.getLocation().y(), 0.0001);
+            assertEquals(0.0, r.getLocation().z(), 0.0001);
+        }
     }
 
     @Test
