1e5a2f31af3392fc0186966755c7ef3207d60458
yiratpeleg <yiratpeleg8@gmail.com>
2020-01-23 14:46:45 +0200

    push

--- src/element/Robots.java
+++ src/element/Robots.java
@@ -153,26 +153,5 @@ public class Robots implements elementFruitRobot {
 
     public static void main(String[] args) {
 
-        Robots[] checkforBuildRobot = new Robots[8];
-        Point3D[] checkPoint3D = new Point3D[8];
-        checkPoint3D[0] = new Point3D(1,2,0);
-        checkPoint3D[1] = new Point3D(3,4,0);
-        checkPoint3D[2] = new Point3D(5,6,0);
-        checkPoint3D[3] = new Point3D(7,8,0);
-        checkPoint3D[4] = new Point3D(9,10,0);
-        checkPoint3D[5] = new Point3D(11,12,0);
-        checkPoint3D[6] = new Point3D(13,14,0);
-        checkPoint3D[7] = new Point3D(15,16,0);
-
-        checkforBuildRobot [0] = new Robots(1,1,1,checkPoint3D[0],1,1,"monkey.png");
-        checkforBuildRobot [1] = new Robots(2,2,2,checkPoint3D[1],2,2,"monkey.png");
-        checkforBuildRobot [2] = new Robots(3,3,3,checkPoint3D[2],3,3,"monkey.png");
-        checkforBuildRobot [3] = new Robots(4,4,4,checkPoint3D[3],4,4,"monkey.png");
-        checkforBuildRobot [4] = new Robots(5,5,5,checkPoint3D[4],5,5,"monkey.png");
-        checkforBuildRobot [5] = new Robots(6,6,6,checkPoint3D[5],6,6,"monkey.png");
-        checkforBuildRobot [6] = new Robots(7,7,7,checkPoint3D[6],7,7,"monkey.png");
-        checkforBuildRobot [7] = new Robots(8,8,8,checkPoint3D[7],8,8,"monkey.png");
-
-
     }
 }
